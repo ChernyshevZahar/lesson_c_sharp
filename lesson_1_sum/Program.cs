dfeb8f45bf7883b0139a8ac65f4279ac88021e34
Lusi <chernyshevzahar@gmail.com>
2023-03-22 19:07:05 +0300

    Создал программу Sum

---
+++ lesson_1_sum/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1 = 	new Random().Next(1,10);
+int num2 = 	new Random().Next(1,10);
+Console.Write("Сумма чисел равна: " + (num2 + num1));
