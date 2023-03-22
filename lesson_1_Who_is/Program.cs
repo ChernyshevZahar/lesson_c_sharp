ac6315becfca7d9818c03d3e03db8f4de70bb836
Lusi <chernyshevzahar@gmail.com>
2023-03-22 19:13:43 +0300

    Создал программу Who_is

---
+++ lesson_1_Who_is/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Напишите ваше имя:");
+String name = Console.ReadLine();
+
+if (name == "Маша")
+{
+    Console.Write( "Рад тебя видет " + name);
+}
+else
+{
+    Console.Write( "Привет " + name);
+}
