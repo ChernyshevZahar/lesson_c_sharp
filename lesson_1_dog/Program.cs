c890100f811ddedc48d3a775ac10d322ccc0773e
Lusi <chernyshevzahar@gmail.com>
2023-03-22 19:33:41 +0300

    Создал программу Dog

---
+++ lesson_1_dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distanse = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int dogspeed = 5;
+int friend = 2;
+int time = 0;
+
+while(distanse > 10)
+{
+    if(friend == 1)
+    {
+        time = distanse/(SecondFriendSpeed+dogspeed);
+        friend = 1;
+    }
+    else
+    {
+        time = distanse/(FirstFriendSpeed+dogspeed);
+        friend = 2;
+    }
+
+    distanse = distanse - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine(count);
+
